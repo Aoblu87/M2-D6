cac1580eacf180d7f6e7750a981aced1ca14fe58
Stefania <stefania.struzzi@gmail.com>
2023-07-10 18:34:14 +0200

    push. ci sono index2 e css2 in aggiunta che devono essere accorpati all'originale

--- script.js
+++ script.js
@@ -1,24 +1,73 @@
-// Il tuo compito è creare un sito e-commerce per Smartphones. 
-// L'homepage che stai per creare sarà la vetrina dei prodotti disponibili, con qualche informazione per ciascun prodotto. 
-// Non sono richieste funzionalità di carrello/cassa.
-// - Completa gli esercizi dall'1 al 6 SOLAMENTE con HTML
-// - Completa gli altri esercizi con JS
 
-//ESERCIZI IN HTML
+// //ESERCIZI IN JS
 
-// ESERCIZIO 1: Inserisci un tag h1 con il nome del tuo negozio
-// ESERCIZIO 2: Aggiungi una tabella con i 5 prodotti più in vista del tuo negozio
-// ESERCIZIO 3: Aggiungi per ogni prodotto un'immagine, un titolo, una descrizione e un prezzo
-// ESERCIZIO 4: Per ogni elemento della tabella aggiungi un link di Amazon al prodotto esistente
-// ESERCIZIO 5: Aggiungi un footer con il nome e l'indirizzo del tuo negozio
-// ESERCIZIO 6: Aggiungi un campo testuale in cui l'utente può lasciare un commento su un prodotto (al momento non serve inserire nessuna "vera" funzionalità di POST/salvataggio!)
 
-//ESERCIZI IN JS
-// ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
-// ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
-// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
-// ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
-// ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
-// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+// // ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
 
+function changeTitle(){
+    document.querySelector('#title').innerHTML='Negozio di arrampicata'}
+ console.log(changeTitle()) 
+ 
+ // // ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
+
+ function changeColorPage() {
+    document.querySelector('body').style.backgroundColor = 'lightblue'
+
+}
+
+console.log(changeColorPage())
+// // ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+
+function changeAddress(){
+    document.querySelector('#address').innerHTML='via Gregorio Ottavo 5, 45332, Roma'
+}
+
+console.log(changeAddress())
+
+
+// // ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+
+function addClass(){
+    
+    let links= document.querySelectorAll('a')
+
+    for(i=0;i<links.length;i++){
+
+    links[i].classList.add('link')}
+}
+
+addClass()
+
+
+
+
+
+// // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della 
+// tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+
+function changeClass(){
+    
+    let img= document.querySelectorAll('.table-cell img')
+
+    for(i=0;i<img.length;i++){
+
+        
+
+    img[i].classList.toggle('imgHidden')}
+}
+
+changeClass()
+
+
+
+// // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto 
+// in uno differente, ogni volta che viene invocata
+
+
+function randomColor(){
+    const prezzi= document.querySelectorAll('#fourth-row p')
+    for(i=0;i<prezzi.length;i++){
+        prezzi[i]
+    }
+}
 
